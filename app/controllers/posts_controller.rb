38cd93f0453a52988e44cb82cc40c0be3acbf065
dounzi <dounzi11@gmail.com>
2018-04-19 21:23:03 +0200

    added CRUD and categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,13 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+if params[:category].blank?
+  @posts = Post.all.order("created_at DESC")
+  else
+  @category_id = Category.find_by(name: params[:category]).id
+  @posts = Post.where(category_id: @category_id).order("created_at DESC")
   end
+end
 
   def show
   end
