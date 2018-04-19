c6246395853662ef990517b4106822a44237417b
dounzi <dounzi11@gmail.com>
2018-04-19 20:16:02 +0200

    Post controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: 'tuto')
+Category.create(name: 'fix')
+Category.create(name: 'idée')
+Category.create(name: 'bon plan')
